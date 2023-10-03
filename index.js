79989ce7bce434a06ca66d500b2d2f386059a33c
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-10-03 07:16:58 -0500

    update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const { title } = require('process');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-// mongoose.connect('mongodb://127.0.0.1/myflixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb://127.0.0.1/myflixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //create write stream
